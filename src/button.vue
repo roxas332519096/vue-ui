48214e16f3dd1e0866fb2a9886e65fe22259df62
roxasdeng <roxas332519096@vip.qq.com>
2019-04-03 10:51:47 +0800

    add parcel

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" ng="scss" scoped>
+    .g-button{
+        padding: 0 1em;
+        height: var(--button-height);
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        font-size: var(--font-size);
+        background: var(--button-bg);
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+
+</style>
